3f63d4d62737a91299352cfa625b160f5e12f210
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-07-18 04:14:54 +0300

    made changes to script.js file

--- script.js
+++ script.js
@@ -6,5 +6,34 @@ document.addEventListener("DOMContentLoaded", function () {
     var username = document.getElementById("username").value.trim();
     var email = document.getElementById("email").value.trim();
     var password = document.getElementById("password").value.trim();
+    var isValid = true;
+    var messages = [];
+    // username validation
+    if (username.length < 3) {
+      isValid = false;
+      messages.push = "Password must contain at least 3 characters";
+    }
+    // email validation
+    if (email.includes("@") && email.includes(".")) {
+      isValid = true;
+    } else {
+      isValid = false;
+      messages.push('Email must contain "@" and "."');
+    }
+    // password validation
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("Password must contain at leats 8 characters");
+    }
+    document.getElementById("feedbackDiv").style.display = "block";
+    if (isValid == true) {
+      document.getElementById("feedbackDiv").textContent =
+        "Registration successful!";
+      document.getElementById("feedbackDiv").style.color = "#28a745";
+    } else {
+      document.getElementById("feedbackDiv").textContent =
+        messages.join("<br>");
+      document.getElementById("feedbackDiv").style.color = "#dc3545";
+    }
   });
 });
