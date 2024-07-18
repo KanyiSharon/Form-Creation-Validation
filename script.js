d7a9ea75ef030bf979fdc0b5eca8e314cd65fde6
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-07-18 03:53:10 +0300

    made changes

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.addEventListener('DOMContentLoaded',
-    function () {
-       const form =document.getElementById("registration-form");
-    })
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var username = document.getElementById("username").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var password = document.getElementById("password").value.trim();
+  });
+});
