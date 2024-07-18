07fe5e3c90faef2ffea83fb40e18ca928e9dae5e
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-07-18 05:03:37 +0300

    made chanegs to fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -1,8 +1,8 @@
 async function fetchUserData() {
-  const apiURL = 'https://jsonplaceholder.typicode.com/users';
+  const apiUrl= 'https://jsonplaceholder.typicode.com/users';
   const dataContainer = document.getElementById("api-data");
   try {
-    const response = await fetch(apiURL);
+    const response = await fetch(apiUrl);
     const users = await response.json();
     dataContainer.innerHTML = "";
     const userList = document.createElement("ul");
